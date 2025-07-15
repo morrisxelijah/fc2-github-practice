fab0f901cb51244813d6cc0f9ef54fd093230bdb
Terry Tilley <terryltilley@gmail.com>
2025-07-15 12:19:08 -0700

    Edit html and script for demo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Future Code 2 GitHub Practice File
 
 console.log('Welcome to GitHub practice!');
-console.log('Try editing this message and pushing your changes.');
+console.log("I think Katy is keeping me honest and I'm here for it!!");
